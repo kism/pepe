feb266bb912741d3f34661037f7336a11f110f29
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-05-14 13:08:37 +0800

    fix

--- piratepepe.py
+++ piratepepe.py
@@ -253,7 +253,7 @@ def download_pepe(url, file_name):
                     else:
                         print("gateway might not have large file support, ", end="")
 
-                    os.remove(file_path)
+                    gw_failure = True
                 except FileNotFoundError:  # Only need to remove partially downloaded file if it exists
                     pass
 
@@ -268,19 +268,19 @@ def download_pepe(url, file_name):
                     except TypeError:
                         gw_failure = True
 
-                if gw_failure:
-                    print("Gateway didn't give us the file")
-                    print(Fore.RED + "removing from gateway list" + Style.RESET_ALL)
-                    os.remove(file_path)
-                    ipfsgatewaylist.remove(gateway)
-                elif os.path.getsize(file_path) == 0:
-                    print(f"Found empty file, removing: {file_name}")
-                    print(f"Gateway: {gateway} {Fore.RED} acting kinda weird{Style.RESET_ALL}...")
-                    os.remove(file_path)
-                else:
-                    print(Back.WHITE + Fore.BLACK + " Success! " + Style.RESET_ALL)
-                    file_downloaded = True
-                    break
+            if gw_failure:
+                print("Gateway didn't give us the file")
+                print(Fore.RED + "removing from gateway list" + Style.RESET_ALL)
+                os.remove(file_path)
+                ipfsgatewaylist.remove(gateway)
+            elif os.path.getsize(file_path) == 0:
+                print(f"Found empty file, removing: {file_name}")
+                print(f"Gateway: {gateway} {Fore.RED} acting kinda weird{Style.RESET_ALL}...")
+                os.remove(file_path)
+            else:
+                print(Back.WHITE + Fore.BLACK + " Success! " + Style.RESET_ALL)
+                file_downloaded = True
+                break
 
             print("trying next gateway...")
     else:
