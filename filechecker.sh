930a7c4f944f413c76fbff70f8692cc2d68103e4
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-05-14 10:48:09 +0800

    fix

--- filechecker.sh
+++ filechecker.sh
@@ -6,6 +6,8 @@ directory="output"
 # Find files and read them into an array
 readarray -d '' files < <(find "$directory" -type f \( -name "*.png" -o -name "*.mp4" -o -name "*.gif" \) -print0)
 
+files=($(for i in "${files[@]}"; do echo $i; done | sort)) # Sort
+
 # Print the array elements
 echo "Checking files..."
 for file in "${files[@]}"; do
