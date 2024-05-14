452f0d69314cb79ae1ddc9fcc9feee1e21715ef1
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-05-14 10:54:33 +0800

    fix

--- filechecker.sh
+++ filechecker.sh
@@ -4,8 +4,10 @@
 directory="output"
 
 # Find files and read them into an array
-readarray -d '' files < <(find "$directory" -type f \( -name "*.png" -o -name "*.mp4" -o -name "*.gif" \) -print0)
+files=()
+borked_files=()
 
+readarray -d '' files < <(find "$directory" -type f \( -name "*.png" -o -name "*.mp4" -o -name "*.gif" \) -print0)
 readarray -t sorted_files < <(printf '%s\n' "${files[@]}" | sort)
 
 # Print the array elements
@@ -14,4 +16,13 @@ for file in "${sorted_files[@]}"; do
     echo
     echo "Checking: $file"
     ffmpeg -v error -i  "$file" -f null -
+    if [ $? -ne 0 ]; then
+        echo "Adding $file to the borked file list"
+        borked_files+=("$file")
+    fi
+done
+
+echo "List of borked files and the commands to run..."
+for file in "${borked_files[@]}"; do
+    echo "rm \"$file\""
 done
