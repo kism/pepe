ffde6f9acec694068b441b82c298841b3fd1cae0
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-05-14 15:08:32 +0800

    fix

--- filechecker.sh
+++ filechecker.sh
@@ -7,15 +7,36 @@ directory="output"
 files=()
 borked_files=()
 
-readarray -d '' files < <(find "$directory" -type f \( -name "*.png" -o -name "*.mp4" -o -name "*.gif" \) -print0)
+echo "Checking files for text when it shouldnt be..."
+readarray -d '' files < <(find "$directory" -type f -print0)
 readarray -t sorted_files < <(printf '%s\n' "${files[@]}" | sort)
-
-# Print the array elements
-echo "Checking files..."
 for file in "${sorted_files[@]}"; do
     echo
     echo "Checking: \"$file\"..."
-    ffmpeg -v error -i  "$file" -f null -
+    if file "$FILE" | grep -q "text"; then
+        echo "Adding \"$file\" to the borked file list"
+        borked_files+=("$file")
+    else
+        echo "Pass!"
+    fi
+
+    ffmpeg -v error -i "$file" -f null -
+    if [ $? -ne 0 ]; then
+        echo "Adding \"$file\" to the borked file list"
+        borked_files+=("$file")
+    else
+        echo "Pass!"
+    fi
+done
+
+echo "Checking AV files with ffmpeg..."
+readarray -d '' files < <(find "$directory" -type f \( -name "*.png" -o -name "*.mp4" -o -name "*.gif" \) -print0)
+readarray -t sorted_files < <(printf '%s\n' "${files[@]}" | sort)
+
+for file in "${sorted_files[@]}"; do
+    echo
+    echo "FFMPEG: checking: \"$file\"..."
+    ffmpeg -v error -i "$file" -f null -
     if [ $? -ne 0 ]; then
         echo "Adding \"$file\" to the borked file list"
         borked_files+=("$file")
