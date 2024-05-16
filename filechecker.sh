2d08776505c2920eccdac034b6179213a1d7eda7
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-05-16 15:35:56 +0800

    fix

--- filechecker.sh
+++ filechecker.sh
@@ -21,21 +21,23 @@ for file in "${sorted_files[@]}"; do
     fi
 done
 
-echo
-echo "-- Checking AV files with ffmpeg --"
-readarray -d '' files < <(find "$directory" -type f \( -name "*.png" -o -name "*.mp4" -o -name "*.gif" \) -print0)
-readarray -t sorted_files < <(printf '%s\n' "${files[@]}" | sort)
 
-for file in "${sorted_files[@]}"; do
-    echo "FFMPEG checking: \"$file\"..."
-    ffmpeg -v error -i "$file" -f null -
-    if [ $? -ne 0 ]; then
-        echo " Adding \"$file\" to the borked file list"
-        borked_files+=("$file")
-    else
-        echo " Pass!"
-    fi
-done
+if [ "$1" == "--quick" ]; then
+    echo
+    echo "-- Checking AV files with ffmpeg --"
+    readarray -d '' files < <(find "$directory" -type f \( -name "*.png" -o -name "*.mp4" -o -name "*.gif" \) -print0)
+    readarray -t sorted_files < <(printf '%s\n' "${files[@]}" | sort)
+    for file in "${sorted_files[@]}"; do
+        echo "FFMPEG checking: \"$file\"..."
+        ffmpeg -v error -i "$file" -f null -
+        if [ $? -ne 0 ]; then
+            echo " Adding \"$file\" to the borked file list"
+            borked_files+=("$file")
+        else
+            echo " Pass!"
+        fi
+    done
+fi
 
 echo "List of borked files and the commands to run..."
 for file in "${borked_files[@]}"; do
